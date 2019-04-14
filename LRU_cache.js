b36739d1f344c1698b1af07495337cbf1c6f01f5
Stephen Chung <me@stephenchung.io>
2019-04-14 15:08:52 -0400

    Complete LRU Cache

--- LRU_cache.js
+++ LRU_cache.js
@@ -1,75 +1,96 @@
 class LinkedListNode {
-  constructor(value = null) {
+  constructor(key = null, value = null) {
+    this.key = key;
     this.value = value;
     this.next = null;
     this.previous = null;
   }
 }
 
-class LinkedList {
-  constructor() {
+class LRUCache {
+  constructor(capacity) {
+    // Create sentinel head and tail nodes to circumvent null checking
     this.head = new LinkedListNode();
     this.tail = new LinkedListNode();
 
+    // Set next and previous of sentinel nodes
     this.head.next = this.tail;
     this.tail.previous = this.head;
-  }
 
-  insert(value) {
-    const newNode = new LinkedListNode(value);
-
-    this.head.next.previous = newNode;
-    newNode.next = this.head.next;
-
-    this.head.next = newNode;
-
-    newNode.previous = this.head;
-
-    return newNode;
-  }
-
-  delete() {
-    this.tail.previous.previous.next = this.tail;
-    this.tail.previous = this.tail.previous.previous;
-  }
-}
-
-class LRUCache {
-  constructor(capacity) {
-    this.linkedList = new LinkedList();
-    this.map = {};
-
+    // Set other class variables
     this.capacity = capacity;
     this.size = 0;
+    this.cache = {};
   }
 
   put(key, value) {
-    if (this.map[key]) {
-      this.map[key].value = value;
-      this.get(key);
+    if (this.cache[key]) {
+      // Delete node from Linked List
+      const node = this.cache[key];
+      node.previous.next = node.next;
+      node.next.previous = node.previous;
+
+      // Reset position in Linked List
+      node.next = this.head.next;
+      this.head.next.previous = node;
+      this.head.next = node;
+      node.previous = this.head;
+
+      // Set node to new value
+      node.value = value;
     } else {
-      this.map[key] = this.linkedList.insert(value);
+      // Create new node with value
+      const newNode = new LinkedListNode(key, value);
+
+      // Add new node to cache
+      this.cache[key] = newNode;
+
+      // Add new node to front of Linked List
+      newNode.next = this.head.next;
+      newNode.previous = this.head;
+      this.head.next.previous = newNode;
+      this.head.next = newNode;
+
+      // Increment size of LRU Cache
       this.size++;
 
-      if (this.capacity < this.size) {
-        delete this.map[this.linkedList.tail.previous.value];
-        this.linkedList.delete();
+      // Check if LRU Cache has exceeded capacity
+      if (this.size > this.capacity) {
+        const tailNode = this.tail.previous;
 
+        // Delete tail node from Linked List
+        tailNode.previous.next = this.tail;
+        this.tail.previous = tailNode.previous;
+
+        // Delete tail node reference from cache
+        delete this.cache[tailNode.key];
+
+        // Update size of LRU Cache
         this.size--;
       }
     }
   }
 
   get(key) {
-    if (this.map[key]) {
-      this.map[key].next.previous = this.map[key].previous;
-      this.map[key].previous.next = this.map[key].next;
-
-      this.linkedList.insert(key);
-
-      return this.map[key].value;
-    } else {
+    if (!this.cache[key]) {
+      // If node does not exist return -1
       return -1;
+    } else {
+      // Access node to move to head;
+      const node = this.cache[key];
+
+      // Delete node from its current position
+      node.next.previous = node.previous;
+      node.previous.next = node.next;
+
+      // Add node to start of Linked List
+      node.previous = this.head;
+      node.next = this.head.next;
+      this.head.next.previous = node;
+      this.head.next = node;
+
+      // Return value
+      return node.value;
     }
   }
 }
